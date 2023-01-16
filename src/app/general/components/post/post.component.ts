7f5ce5076fd780f409353346f887c43181509412
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2023-01-16 17:42:17 -0500

    Crear comentario, hecho

--- src/app/general/components/post/post.component.ts
+++ src/app/general/components/post/post.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/model/post.model';
 import { PostService } from 'src/app/services/post.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommentsService } from 'src/app/services/comments.service';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -13,27 +15,45 @@ export class PostComponent implements OnInit {
     id: 0,
     title: '',
     content: '',
-
   };
+  form: FormGroup
   comments: any = []; // nueva propiedad para almacenar los comentarios
+  commentContent: string = '';
   constructor(
     private postService: PostService,
     private route: ActivatedRoute,
     private commentsService: CommentsService, //injectamos el servicio de comentarios,
+    private formBuilder: FormBuilder,
+    private router: Router
 
   ) {
+    this.form=this.formBuilder.group({
+      content: ['',]
+    })
   }
 
   ngOnInit(): void {
      this.route.params
-      .subscribe(params => {
+      .subscribe(params => {  
         const id = params['id']
         this.postService.getPost(id)
-          .subscribe(data => this.post = data)
-        
+          .subscribe(data =>
+            this.post = data
+            )
         this.commentsService.getCommentsById(id)
           .subscribe(data => this.comments = data) // asignamos los comentarios a la propiedad del componente
       })
 }
+
+createComment() {
+  const commentData = this.form.value;
+  this.commentsService.createComment(this.post.id, commentData)
+
+  .subscribe(response => {    
+    this.router.navigate(['/post'])
+    // manejar la respuesta del servidor
+  });
+}
+
 
 }
--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Comment } from '../model/comment.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +12,10 @@ export class CommentsService {
 
   getCommentsById(id: number){
     return this.http.get(`http://127.0.0.1:5000/posts/${id}/comments`)
+  }
+
+  createComment(id: number, comment: Comment){
+    return this.http.post<Comment[]>(`http://127.0.0.1:5000/posts/${id}/comments`, comment)
   }
 
 }
