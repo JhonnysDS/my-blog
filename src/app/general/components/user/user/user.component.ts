3e0e35d6b830ea2ac94479b64e882cca1204f88a
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2023-03-16 14:34:22 -0500

    Mostrar avatar en información del perfil. hacho

--- src/app/general/components/user/user/user.component.ts
+++ src/app/general/components/user/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import jwt_decode from 'jwt-decode';
+import { FileUpload } from 'src/app/model/fileUpload.model';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -10,11 +11,12 @@ export class UserComponent implements OnInit {
   userId: number = 0;
   username: string = ''
   email: string = ''
-  created: string = ''
-  editUser: boolean = true
-  constructor(private authService: AuthService) { 
-  
-  
+  avatar: FileUpload | string | null = null;
+  editUser: boolean = false
+  avatarUrl = 'http://localhost:5000/static/images/profile/'
+  constructor(private authService: AuthService) {
+
+
   }
 
   ngOnInit(): void {
@@ -22,22 +24,25 @@ export class UserComponent implements OnInit {
     if (token) {
       const decodedToken: any = jwt_decode(token);
       this.userId = decodedToken.id;
-      
+
     }
 
     this.getDatasUser()
   }
 
 
-  getDatasUser(){
+  getDatasUser() {
     this.authService.foundUser(this.userId)
     .subscribe(response => {
-      this.username = response.username
-      this.email = response.email
-      this.created = response.created_at
-      
-    })
-    
+      this.username = response.username;
+      this.email = response.email;
+      const avatarString = response.avatar.replace(/'/g, '"').replace(/True/g, 'true');
+      this.avatar = JSON.parse(avatarString);
+      if (this.avatar && typeof this.avatar === 'object' && this.avatar.imageServer) {
+        this.avatar = `${this.avatar.imagePath}${this.avatar.imageExt}`;
+      }
+    });
+
   }
 
 
